637c062ff39f19a4a12959b21cbf93f63ac6c81c
vishwaridha-s <vishwa4008s@gmail.com>
2025-08-02 09:51:48 +0530

    voyo

---
+++ src/main/java/com/example/travellapp/models/placesModel.java
@@ -0,0 +1,24 @@
+package com.example.travellapp.models;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.ArrayList;
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class placesModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String location;
+    private String description;
+    private int price;
+    private String imageName;
+    private String imageType;
+    @Lob
+    private byte[] imageData;
+}
