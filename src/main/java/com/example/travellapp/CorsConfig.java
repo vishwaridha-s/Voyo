1f6ded2a553f5602cb917185ca614de58ad196c2
vishwaridha-s <vishwa4008s@gmail.com>
2025-08-02 12:06:54 +0530

    deploy

--- src/main/java/com/example/travellapp/CorsConfig.java
+++ src/main/java/com/example/travellapp/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://voyoreact.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
